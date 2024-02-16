522abf28809689a766bcc6793f006cbe527b28e1
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2024-02-16 08:39:55 +0100

    Added all loss functions

--- training.py
+++ training.py
@@ -11,11 +11,27 @@ from torch.utils.data import DataLoader
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 LOSS_FN_TYPES = {
-    "crossentropy": nn.CrossEntropyLoss,
-    "mse": nn.MSELoss,
     "l1": nn.L1Loss,
+    "smoothl1": nn.SmoothL1Loss,
+    "mse": nn.MSELoss,
+    # "ctc": nn.CTCLoss, FIX: requires 2 positional arguments
     "nll": nn.NLLLoss,
-    "bce": nn.BCELoss
+    "poissonnll": nn.PoissonNLLLoss,
+    # "gaussiannll": nn.GaussianNLLLoss, FIX requires a positional argument
+    "kldiv": nn.KLDivLoss,
+    "bce": nn.BCELoss,
+    "bcelogits": nn.BCEWithLogitsLoss,
+    # "marginranking": nn.MarginRankingLoss, FIX requires a positional argument
+    "hingeembedding": nn.HingeEmbeddingLoss,
+    "multimargin": nn.MultiMarginLoss,
+    "multilabelmargin": nn.MultiLabelMarginLoss,
+    "multilabelsoftmargin": nn.MultiLabelSoftMarginLoss,
+    "huber": nn.HuberLoss,
+    "softmargin": nn.SoftMarginLoss,
+    # "cosineembedding": nn.CosineEmbeddingLoss, FIX requires a positional argument
+    "crossentropy": nn.CrossEntropyLoss,
+    # "tripletmargin": nn.TripletMarginLoss, FIX requires a positional argument
+    # "tripletmargindistance": nn.TripletMarginWithDistanceLoss, FIX requires a positional argument
 }
 
 OPTIMIZER_TYPES = {
@@ -27,7 +43,7 @@ OPTIMIZER_TYPES = {
     "adamax": optim.Adamax,
     "asgd": optim.ASGD,
     "sgd": optim.SGD,
-    # "lbfgs": optim.LBFGS,  FIX: not usable until closure parameter is added
+    # "lbfgs": optim.LBFGS,  FIX: requires a positional argument
     "nadam": optim.NAdam,
     "radam": optim.RAdam,
     "rmsprop": optim.RMSprop,
