b102b31c1b69376aa78858a2c5281875850806b6
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2024-02-19 09:57:01 +0100

    Added data scripts for multiple learning rates analysis

---
+++ data-collection/multiple-learning-rates.sh
@@ -0,0 +1,6 @@
+if [ $# -ne 1 ]; then
+	echo "Enter a number of epochs as the first positional argument."
+	exit -1
+fi
+
+parallel ./main.py avg -ep $1 -lr {} -lor 0.01 ::: 0.1 0.05 0.025 0.01 0.005 0.0025 0.0001
